a2ad7de6b5db1d2f44fafb6f84897f5306f5fad3
I Ketut Wisnadiputra (210040138) <140131629+wisnadiputra1@users.noreply.github.com>
2024-04-11 10:38:30 +0800

    Update InputSearch.jsx

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,24 +1,31 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const InputSearch = () => {
 
     
-    const router =useNavigate()
-   
-    const handleKeyDown = async (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            if(e.target.value === ''){
+    const [searchTerm, setSearchTerm] = useState('');
+    const router = useNavigate();
+
+  const handleKeyDown = async (e) => {
+      if (e.key === 'Enter') {
+          e.preventDefault();
+          if (searchTerm.trim() === '') {
               return;
-            }
-            router(`/search/${e.target.value}`)
-        }
-    };
+          }
+          router(`/search/${searchTerm}`);
+          setSearchTerm('');
+      }
+  };
+
+  const handleInputChange = (e) => {
+      setSearchTerm(e.target.value);
+  };
 
   return (
     <div className=" lg:w-max md:w-max w-screen mx-3 my-2">
          <form  action="">
-            <input type="text" placeholder="Search anime..." className="outline-none py-1 px-2 w-full" autoFocus onKeyDown={handleKeyDown} required />
+            <input type="text" placeholder="Search anime..." value={searchTerm} className="outline-none py-1 px-2 w-full" autoFocus onChange={handleInputChange} onKeyDown={handleKeyDown} required />
         </form>
     </div>
        
