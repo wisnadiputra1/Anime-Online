0525f8d5059de510fd0c601c79e2f5d9323cb163
I Ketut Wisnadiputra (210040138) <140131629+wisnadiputra1@users.noreply.github.com>
2024-04-11 10:47:01 +0800

    Update Search.jsx

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Card from "../../components/Card";
+import { Link } from "react-router-dom";
 import {ChevronLeft, ChevronRight} from 'lucide-react'
 
 const Search = () => {
@@ -52,7 +52,29 @@ const Search = () => {
         </div>
 
         <div className="mb-1 mt-3 mx-5 grid grid-cols-2 md:grid-cols-5 lg:grid-cols-5 lg:w-full">
-            {anims.length === 0 ? <h2 className="text-white col mx-auto text-lg ">No anime contains {query}</h2> : <Card datas={anims} />}
+            {anims.length === 0 ? <h2 className="text-white col mx-auto text-lg ">No anime contains {query}</h2> :  anims && anims.map((data) => (
+              <Link key={data.mal_id} to={`/anime/${data.mal_id}`} className="max-w-[120px] max-h-[200px] md:max-h-[220px] md:max-w-[140px] mb-10 group">
+                <div className="relative cursor-pointer  overflow-hidden">
+                  <p className="w-max h-max absolute z-10 px-2 text-white bg-black bg-opacity-70 text-sm">
+                    Episodes: {data.episodes}
+                  </p>
+                  <p className="w-max h-max absolute z-10 right-0 top-[90px] flex items-center justify-around gap-1 bg-blue-500 bg-opacity-90 text-white px-1 rounded-s-md">
+                    <img src="./Firee.png" alt="Firee" width="12px" />
+                    {data.score}
+                  </p>
+                  {data.images && data.images.webp && (
+                    <img
+                      src={data.images.webp.image_url}
+                      alt="Anime"
+                      className="w-[120px] h-[200px] md:w-[140px] md:h-[220px] "
+                    />
+                  )}
+                  <h2 className={`z-20 w-[120px]  h-[70px] md:w-[140px] md:h-[90px] text-center absolute bottom-[0.1px] mx-auto text-white bg-black bg-opacity-70 px-2 py-1 text-sm line-clamp-1 translate-y-[70%] group-hover:translate-y-[0] group-hover:line-clamp-3 transition duration-500`} >
+                    {data.title}
+                  </h2>
+                </div>
+              </Link>
+            ))}
           
         </div>
 
